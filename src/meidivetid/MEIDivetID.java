b6401bc003baf85e62a2bdf33728ef5a3232e1e8
Derek Bickhart <njdbickhart@gmail.com>
2015-12-21 16:35:14 -0500

    added logic to save entries with no surrounding repea

--- src/meidivetid/MEIDivetID.java
+++ src/meidivetid/MEIDivetID.java
@@ -71,7 +71,7 @@ public class MEIDivetID {
             for(String c : SortByChr.ascendingChr(chrs)){
                 for(BedClosest.BedCompare b : results.getSortedBedAbstractList(c)){
                     List<String> values = b.getOutStringList(true);
-                    if(Integer.parseInt(values.get(8)) < 1000){
+                    if(Integer.parseInt(values.get(8)) < 1000 && (Integer.parseInt(values.get(6)) != 0 && !values.get(7).equals("NA"))){
                         failed.write(StrUtils.StrArray.Join((ArrayList<String>)values, "\t") + System.lineSeparator());
                         continue;
                     }
